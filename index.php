2aeee45723599b5da6d62358bfe38d85434ebd50
Vo Huy Khoa <huykhoa630@gmail.com>
2022-11-12 15:43:15 +0700

    update file css

--- index.php
+++ index.php
@@ -24,65 +24,9 @@ if (file_exists($path)) {
 
 <base href="<?php echo Helper::get_url(''); ?>">
 
-<!-- Google Font -->
-<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
-<style>
-    a:hover {
-        color: red !important;
-    }
-    @media only screen {
-        .col-lg-8 form {
-            margin-top: 36px;
-        }
-    }
-    /* Tablet Device = 768px */
-    @media only screen and (min-width: 768px) and (max-width: 991px) {
-        .col-lg-8 form {
-            margin-top: -65px;
-            width: 60%;
-            margin-left: 130px;
-        }
-    }
-    /* Wide Mobile = 480px */
-    @media only screen and (max-width: 767px) {
-        .col-lg-8 form {
-            margin-top: -12px;
-            width: 90%;
-            margin-left: 15px;
-        }
-        .anime-details {
-            margin-top: -50px !important;
-            width: 50% !important;
-        }
-        .product__item__pic {
-            height: 250px !important;
-        }
-        .hero__items {
-            height: 250px !important;
-        }
-    }
-    /* Small Device = 320px */
-    @media only screen and (max-width: 479px) {
-        .hero__items {
-            height: 50px !important;
-        }
-    }
-</style>
 
-<!-- Css Styles -->
-<link rel="stylesheet" href="public/user/css/bootstrap.min.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/font-awesome.min.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/elegant-icons.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/plyr.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/nice-select.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/owl.carousel.min.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/slicknav.min.css" type="text/css">
-<link rel="stylesheet" href="public/user/css/style.css" type="text/css">
 
-<!-- font awesome -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
 
 <!-- Js Plugins -->
 <script src="public/user/js/jquery-3.3.1.min.js"></script>
--- view/common/home.php
+++ view/common/home.php
@@ -8,7 +8,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hoạt Hình Anime Hay</title>
-
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="public/user/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/plyr.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="public/user/css/style.css" type="text/css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+    
+<style>
+    a:hover {
+        color: red !important;
+    }
+    @media only screen {
+        .col-lg-8 form {
+            margin-top: 36px;
+        }
+    }
+    /* Tablet Device = 768px */
+    @media only screen and (min-width: 768px) and (max-width: 991px) {
+        .col-lg-8 form {
+            margin-top: -65px;
+            width: 60%;
+            margin-left: 130px;
+        }
+    }
+    /* Wide Mobile = 480px */
+    @media only screen and (max-width: 767px) {
+        .col-lg-8 form {
+            margin-top: -12px;
+            width: 90%;
+            margin-left: 15px;
+        }
+        .anime-details {
+            margin-top: -50px !important;
+            width: 50% !important;
+        }
+        .product__item__pic {
+            height: 250px !important;
+        }
+        .hero__items {
+            height: 250px !important;
+        }
+    }
+    /* Small Device = 320px */
+    @media only screen and (max-width: 479px) {
+        .hero__items {
+            height: 50px !important;
+        }
+    }
+</style>
 
 </head>
 

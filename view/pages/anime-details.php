55e4a277c400ab39ebcc26d446c56717e17127a3
KhoaVH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2023-03-16 13:42:48 +0700

    clean code

--- view/pages/anime-details.php
+++ view/pages/anime-details.php
@@ -1,36 +1,4 @@
- <?php
-
-    $id = filter_input(INPUT_GET, 'anime_id');
-
-    // categories
-    $sql_categories = "select * from categories";
-    $list_categories = Database::db_get_list($sql);
-
-    // row anime
-    $sql = "select * from animes where id = :id";
-    $params = [
-        'id' => $id
-    ];
-    $anime = Database::db_get_row($sql, $params);
-
-    // list video
-    $sql_video = "select * from videos where anime_id = :id";
-    $list_video = Database::db_get_list_condition($sql_video, $params);
-
-    // view
-    $sql_view = "UPDATE animes SET view = view + 1 WHERE id = :id";
-    Database::db_execute($sql_view,$params);
-
-
-    // max sort
-    $sql_max = "select max(sort) from videos where anime_id = :id";
-    $param_max = [
-        'id' => $id
-    ];
-    $anime_max = Database::db_get_row($sql_max, $param_max);
-
-?>
-
+<?php include('/controllers/user/anime-detail.php') ?>
 
  <!-- Anime Section Begin -->
  <section class="anime-details spad">

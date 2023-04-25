595e998aa09bdb50fb4c4a465e507b9b82df1452
KhoaVH <78124749+Vo-Huy-Khoa@users.noreply.github.com>
2023-04-25 14:21:19 +0700

    Update helper.php

--- model/helper.php
+++ model/helper.php
@@ -198,7 +198,7 @@ class Helper{
 
 class Database{
 
-    private static $dsn = "mysql:host=http://18.181.241.67/;dbname=anime";
+    private static $dsn = "mysql:host=http://localhost;dbname=anime";
     private static $username = "root";
     private static $password = "123456";
     private static $con = null;
